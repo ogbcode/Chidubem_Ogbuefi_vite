1cdd67d3c6bc86697e6b13967f379fccd3125a3d
OGB <chidubem.info@gmail.com>
2024-01-21 22:22:00 +0100

    first commit

--- hms-apps/api-gateway/src/main.ts
+++ hms-apps/api-gateway/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'v', //this is actually the default, if not indicated
+    //defaultVersion: '1' In case you need a global version tagging where there is no explicitly declared version
+    // or
+    //defaultVersion: ['1', '2']
+    // or
+    //defaultVersion: '1' 
+  });
   await app.listen(3002);
 }
 bootstrap();
